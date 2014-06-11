3d78150bef2370fd0b545d20e4c7828772822b93
spiridenok <spiridenok@tut.by>
2014-06-11 16:04:54 +0200

    Update MyWizard.java

--- wizard/MyWizard.java
+++ wizard/MyWizard.java
@@ -1,12 +1,14 @@
 package wizard;
 
+import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.jface.wizard.WizardDialog;
 
 public class MyWizard extends Wizard {
 
-	protected SourceSelectionPage one;
-	protected AnalysisResultPage two;
+	protected SourceSelectionPage source_selection_page = new SourceSelectionPage();
+	protected AnalysisResultPage analysis_result_page = new AnalysisResultPage();
+	protected OptionalPage optional_page = new OptionalPage();
 	
 	private String[] result = { "Result 1", "Result 2", "End Result" };
 
@@ -20,16 +22,25 @@ public class MyWizard extends Wizard {
 		return "Export My Data";
 	}
 
-	
 	@Override
 	public void addPages() {
-		addPage(new SourceSelectionPage());
-		addPage(new AnalysisResultPage());
+		addPage(source_selection_page);
+		addPage(optional_page);
+		addPage(analysis_result_page);
 	}
 
 	@Override
 	public boolean performFinish() {
 		// Print the result to the console
 		return true;
+	}
+	
+	@Override
+	public IWizardPage getNextPage(IWizardPage page) {
+		if( false )
+			return optional_page;
+		if( page == source_selection_page )
+			return analysis_result_page;
+		return super.getNextPage(page);
 	}
 }
