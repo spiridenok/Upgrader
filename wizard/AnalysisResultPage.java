eac12515155467f1be3fed9a7c2e0293338f14d9
spiridenok <spiridenok@tut.by>
2014-06-12 10:06:24 +0200

    Update AnalysisResultPage.java

--- wizard/AnalysisResultPage.java
+++ wizard/AnalysisResultPage.java
@@ -2,32 +2,17 @@ package wizard;
 
 import javax.swing.GroupLayout.Alignment;
 
-import org.eclipse.jface.wizard.WizardPage;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.jface.wizard.WizardPage;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.KeyEvent;
-import org.eclipse.swt.events.KeyListener;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.swt.widgets.TableItem;
-import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.widgets.Table;
-
 public class AnalysisResultPage extends WizardPage {
 
 	private Composite container;
 	private Table table;
-
-	public AnalysisResultPage() {
-
+	private ChangeModel model;
+	
+	public AnalysisResultPage( ChangeModel m ) {
 		super("Second Page");
 		setTitle("Results of constants analysis");
 		setDescription("Followng are result of automatic analysis of constants DDF files");
+		model = m;
 	}
 
 	@Override
@@ -42,7 +27,7 @@ public class AnalysisResultPage extends WizardPage {
 		table = new Table(container, SWT.BORDER | SWT.FULL_SELECTION);
 
 		// For some reason set width on a column does not work...
-		String[] titles = {"Changed item", "Change type",  "Upgrader?"};
+		String[] titles = {"Changed item", "Change type",  "Upgrader Type", "Patch scripts"};
 		for (int i=0; i<titles.length; i++) {
 			TableColumn column = new TableColumn (table, SWT.NONE);
 //			column.setAlignment(SWT.CENTER);
@@ -50,24 +35,35 @@ public class AnalysisResultPage extends WizardPage {
 		}	
 		
 		TableItem item = new TableItem (table, SWT.NONE);
-		item.setText(0, "tc_main.bla.data.something");
+		item.setText(0, "top_level.bla.data.something");
 		item.setText(1, "default");
-		item.setText(2, "NO");
+		item.setText(2, "None");
+		item.setText(3, "Yes");
 		
 		item = new TableItem (table, SWT.NONE);
-		item.setText(0, "tc_main.something_totally_different.test");
+		item.setText(0, "top_level.something_totally_different.test");
 		item.setText(1, "type");
-		item.setText(2, "YES");
-
+		item.setText(2, "Copy");
+		item.setText(3, "No");
+		
+		item = new TableItem (table, SWT.NONE);
+		item.setText(0, "top_level.some_different.struct.changed");
+		item.setText(1, "scale");
+		item.setText(2, "Copy");
+		item.setText(3, "Yes");
+		
 		for (int i=0; i<titles.length; i++) {
 			table.getColumn(i).pack ();
 		}
 		
-		GridData gd_table = new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1);
-		gd_table.heightHint = 182;
+		GridData gd_table = new GridData(SWT.FILL, SWT.CENTER, true, true, 1, 1);
+		gd_table.heightHint = 192;
 		table.setLayoutData(gd_table);
 		table.setHeaderVisible(true);
 		table.setLinesVisible(true);
+		
+		Label lblNewLabel = new Label(container, SWT.NONE);
+		lblNewLabel.setText("Pressing finish will start process of generation of the required code, this might take some time...");
 		
 		setPageComplete(true);
 	}
