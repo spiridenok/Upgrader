b82f67418a9b81aca7e11133b5bb00f5cc83c1c2
spiridenok <spiridenok@tut.by>
2014-07-03 17:07:07 +0200

    Update ChangeModel.java

--- wizard/ChangeModel.java
+++ wizard/ChangeModel.java
@@ -1,166 +1,121 @@
 package wizard;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-public class CodeGenerator {
-
-	private final String SRC_GEN_DIR = "src-gen";
-	private final String PATCH_GEN_DIR = "patch-gen";
+public class ChangeModel {
 	
-	private void delay() {
-		System.out.println("1");
-		final int sec = 1;
-		try {
-			Thread.currentThread();
-			Thread.sleep(sec * 1000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-//		System.out.println("2");
+	private ParseDacnf dacnf;
+	private List<String> changed_defaults = new ArrayList<String>();
+	private List<String> changed_var_arr_len = new ArrayList<String>();
+	private List<String> changed_const_types = new ArrayList<String>();
+	private List<String> changed_data_types = new ArrayList<String>();
+	private Set<String> unresolved_ddfs = new HashSet<String>();
+
+	public void calculate_difference( String old_files_path, String new_files_path, String dacnf_file_name ) {
+		System.out.println("Calculating differences started...");
+		changed_defaults.clear();
+		changed_const_types.clear();
+		changed_data_types.clear();
+		unresolved_ddfs.clear();
+		
 //		try {
 //			Thread.currentThread();
-//			Thread.sleep(sec * 1000);
+//			Thread.sleep(1 * 1000);
 //		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
 //			e.printStackTrace();
 //		}
-//		System.out.println("3");
-//		try {
-//			Thread.currentThread();
-//			Thread.sleep(sec * 1000);
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
-		System.out.println("4");
+		
+		dacnf = new ParseDacnf(dacnf_file_name);
+		System.out.println( "Data type " + dacnf.get_const_data_type() );		
+		
+		// TODO: clean this up - this constructor is one big side-effect.
+		// Make it implementation less surprising for developers. 
+		ManualDiff diff = new ManualDiff( this, old_files_path, new_files_path, dacnf.get_const_data_type() );
+		
+		System.out.println("Calculating differences finished.");
 	}
-	
-	private void generate_src_struct()
+
+	public boolean changed_scales() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean changed_len_var_array() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean need_specific_upgrader() {
+		return !changed_data_types.isEmpty();
+	}
+
+	boolean changed_type_and_default() {
+		return !changed_defaults.isEmpty() && !changed_const_types.isEmpty();
+	}
+
+	String get_const_file_name()
 	{
-		new File( SRC_GEN_DIR ).mkdir();
+		return dacnf.get_const_file_name();
 	}
-	
-	private void gen_update_sh_script( String upgrader_name)
+
+	public void add_changed_default( String var )
 	{
-		try( PrintWriter writer = new PrintWriter( SRC_GEN_DIR + "/" + upgrader_name, "UTF-8") )
-		{
-			writer.println("#!/bin/sh");
-			writer.println( String.format("python -m %s 2> /dev/null", upgrader_name) );
-			writer.close();		
-		}	
-		catch( IOException ex ) {
-			ex.printStackTrace();
-		}
+		changed_defaults.add(var);
 	}
 	
-	private void gen_update_py_script()
+	public void add_changed_var_array_len( String var )
 	{
-		try( PrintWriter writer = new PrintWriter( SRC_GEN_DIR + "/CCBB_upgrade.py", "UTF-8") )
-		{
-			writer.println("The first line");
-			writer.println("The second line");
-			writer.close();		
-		}	
-		catch( IOException ex ) {
-			ex.printStackTrace();
-		}
+		changed_var_arr_len.add(var);
 	}
 	
-	private void gen_pre_patch_script( String const_file_name ) {
-	    try {
-	    	String pre_patch_filename = PATCH_GEN_DIR + "/prepatch";
-	    	// TODO: should not be copy but append to the pre-patch file.
-			Files.copy( new File("src/PrepatchBackupFile.sh").toPath(), 
-						new File(pre_patch_filename).toPath() );
-			
-			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(pre_patch_filename, true)));
-		    out.println("\nprepatch_backup_file \"${ASMHOME}\"" + const_file_name );
-		    out.println("echo \"Backed up " + const_file_name + " file before patch install.\"");
-		    out.close();			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	    
-	    try {
-	    	String post_backout_filename = PATCH_GEN_DIR + "/postbackout";
-	    	// TODO: should not be copy but append to the post-backout file.
-			Files.copy( new File("src/PostbackoutRestoreBackupFile.sh").toPath(), 
-						new File(post_backout_filename).toPath() );
-			
-			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(post_backout_filename, true)));
-		    out.println("\npostbackout_restore_backup \"${ASMHOME}\"" + const_file_name );
-		    out.println("echo \"Restored after patch backout " + const_file_name + " file that was saved before patch install.\"");
-		    out.close();			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public void add_changed_const_type( String var )
+	{
+		changed_const_types.add(var);
 	}
 	
-	public void generate( ChangeModel model ) {
-
-		removeDirectory(new File(PATCH_GEN_DIR));
-		removeDirectory(new File(SRC_GEN_DIR));
-		
-		if (model.changed_type_and_default())
-		{
-			new File( PATCH_GEN_DIR ).mkdir();
-			// TODO: const file name must be retrieved from the real dacnf file
-			gen_pre_patch_script( model.get_const_file_name() );
-		}
-
-		if( model.need_specific_upgrader() )
-		{
-			generate_src_struct();
-			// TODO: upgrader script must be retrieved from the wizard
-			gen_update_sh_script( "CCBB_upgrade" );
-			gen_update_py_script();
-		}
-		
-		if( model.changed_len_var_array() )
-			generate_src_struct();
-		
-		if( model.changed_scales() )
-			generate_src_struct();
-		
-		//generate_test_doc();
-		
-		// TODO: remove delay later
-		delay();
+	public void add_changed_data_type( String var )
+	{
+		changed_data_types.add(var);
 	}
 	
-	private static boolean removeDirectory(File directory) {
-		if (directory == null)
-			return false;
-		if (!directory.exists())
-			return true;
-		if (!directory.isDirectory())
-			return false;
-
-		String[] list = directory.list();
-
-		// Some JVMs return null for File.list() when the
-		// directory is empty.
-		if (list != null) {
-			for (int i = 0; i < list.length; i++) {
-				File entry = new File(directory, list[i]);
-
-				// System.out.println("\tremoving entry " + entry);
-
-				if (entry.isDirectory()) {
-					if (!removeDirectory(entry))
-						return false;
-				} else {
-					if (!entry.delete())
-						return false;
-				}
-			}
+	public void add_unresolved_ddf( String ddf_file )
+	{
+//		System.out.println( "add unresolved " + ddf_file );
+		unresolved_ddfs.add(ddf_file);
+	}
+	
+	public List<DisplayTableData> get_changes()
+	{
+		List<DisplayTableData> data = new ArrayList<DisplayTableData>();
+		for( String var: changed_defaults )
+		{
+			data.add( new DisplayTableData( var, "default", "None", false ) );
 		}
+		for( String var: changed_const_types )
+		{
+			data.add( new DisplayTableData( var, "const type", "None", true ) );
+		}
+		for( String var: changed_data_types )
+		{
+			data.add( new DisplayTableData( var, "data type", "Copy", false ) );
+		}
+		for( String var: changed_var_arr_len )
+		{
+			data.add( new DisplayTableData( var, "var array len", "VarArrayLen", false ) );
+		}
+		
+		return data;
+	}
 
-		return directory.delete();
+	public boolean has_unresolved_ddfs() {
+		return !unresolved_ddfs.isEmpty();
+	}
+
+	public Set<String> get_unresolved_ddfs() {
+		return unresolved_ddfs;
 	}
 }
